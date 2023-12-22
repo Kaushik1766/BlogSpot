c719be2cf1c03240352a741d75ac0396e5b6927c
Kaushik Saha <22bcs14861@cuchd.in>
2023-12-23 02:25:44 +0530

    added backend auth

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from 'cors';
 import pg from 'pg';
+import bodyParser from "body-parser";
+import bcrypt from 'bcrypt';
 
+const saltRounds = 4;
 const app = express();
 app.use(cors());
 const port = 3000;
-app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const db = new pg.Client({
     user: "postgres",
@@ -17,18 +20,47 @@ const db = new pg.Client({
 
 db.connect();
 
-let content = [
-    {
-        title: "Blue strands in the Sands",
-        post: "Mauritania is blue. That sounds strange perhaps. Most people, if they think of the country at all, or know about it for that matter, might say it is more sand coloured. It is 90% desert after all. And sand is everywhere. The desert never far away. Nouadibhou, Mauritinia’s second largest city, feels more like a conglomeration of desert villages then a city. There are a few paved roads, but most of it is rutted tracks, swathes of open sandy patches, low mud brick dwellings, goats, camels, Bedouin tents, and lots and lots of garbage. Most people would say it’s a shithole. I liked it. Mainly because it just isn’t like any other city I have been to. And the fishing port is very interesting. You aren’t allowed to take pictures for some reason, but you are free to stroll around and watch the hustle and bustle that surrounds it. It’s colourful, a mix of Senegalese fishermen and Mauritanian buyers. Colourful not only in dress, but also in skin. From darker tones to lighter ones.",
-        author: "Ralf Kreuze",
-    },
-    {
-        title: "Blue strands in the Sands",
-        post: "Mauritania is blue. That sounds strange perhaps. Most people, if they think of the country at all, or know about it for that matter, might say it is more sand coloured. It is 90% desert after all. And sand is everywhere. The desert never far away. Nouadibhou, Mauritinia’s second largest city, feels more like a conglomeration of desert villages then a city. There are a few paved roads, but most of it is rutted tracks, swathes of open sandy patches, low mud brick dwellings, goats, camels, Bedouin tents, and lots and lots of garbage. Most people would say it’s a shithole. I liked it. Mainly because it just isn’t like any other city I have been to. And the fishing port is very interesting. You aren’t allowed to take pictures for some reason, but you are free to stroll around and watch the hustle and bustle that surrounds it. It’s colourful, a mix of Senegalese fishermen and Mauritanian buyers. Colourful not only in dress, but also in skin. From darker tones to lighter ones.",
-        author: "Ralf Kreuze",
-    },
-]
+app.post("/register", async (req, res) => {
+    let { username, password } = req.body;
+    if ((await db.query(`select * from users where username = '${username}'`)).rows[0] == null) {
+        bcrypt.hash(password, saltRounds, async (err, hashedPassword) => {
+            if (err) {
+                console.log("error" + err);
+                res.send("Error occured");
+            }
+            else {
+                await db.query(`insert into users(username, password) values('${username}','${hashedPassword}')`);
+                res.send("Registered successfully");
+            }
+        })
+    }
+    else {
+        res.send("username already taken");
+    }
+});
+
+app.post("/login", async (req, res) => {
+    let { username, password } = req.body;
+    let hashedPassword = ((await db.query(`select * from users where username = '${username}' `)).rows[0]);
+
+    if (hashedPassword == null) {
+        res.send("user not registered");
+    }
+    else {
+        bcrypt.compare(password, hashedPassword.password, (err, result) => {
+            if (err) {
+                console.log(err);
+                res.send("Error occured");
+            }
+            if (result == false) {
+                res.send("Wrong password");
+            }
+            else {
+                res.send("Logged in successfully.");
+            }
+        })
+    }
+})
 
 app.get('/', (req, res) => {
     db.query("select * from blog").then((resp) => {
